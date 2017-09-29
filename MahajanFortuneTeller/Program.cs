343ae8c7bebaf81fe38b4ff7deb67ca4359ca222
mahajanl <lakshamima@gmail.com>
2017-09-29 13:34:24 -0400

    a whole lot more else if statements for part II

--- MahajanFortuneTeller/Program.cs
+++ MahajanFortuneTeller/Program.cs
@@ -16,7 +16,6 @@ namespace MahajanFortuneTeller
             Console.WriteLine("If you'd like to gaze into the future, I can help you with that!");
             Console.WriteLine("Enter your first name for the Great Elvira.");
             string userFirstName = Console.ReadLine().ToLower().Trim();
-            Console.WriteLine(userFirstName);
 
             Console.WriteLine("And now can I have your last name? Promise I won't google you.");
             string userSecondName = Console.ReadLine().ToLower().Trim();
@@ -24,17 +23,18 @@ namespace MahajanFortuneTeller
             Console.WriteLine("How about your age? I'll never tell another... living... soul!");
             int userAge = int.Parse(Console.ReadLine().Trim());
 
-            string retire;
-                if (userAge % 2 == 0)
-                { retire = "5";
-                }
-                else if (userAge % 2 != 0)
-                { retire ="2";
-                }
+                string retire;
+                    if (userAge % 2 == 0)
+                    { retire = "5";
+                    }
+                    else 
+                    { retire ="2";
+                    }
        
             Console.WriteLine("If I could also have your birth month as a NUMBER 1-12.");
             Console.WriteLine("Not that I don't already KNOW... I'm just bad with number conversion.");
             int userMonth = int.Parse(Console.ReadLine().Trim());
+                
 
             Console.WriteLine("And now.. tell me your MOST FAVORITE of the colors of the ROYGBIV rainbow!");
             Console.WriteLine("If you don't know what ROYGBIV stands for, let me know by entering \"HELP.\"");
@@ -42,30 +42,55 @@ namespace MahajanFortuneTeller
                 if (userFavColor == "help")
                 { Console.WriteLine("R O Y G B I V stands for the colors of the rainbow as \nRed, Orange, Yellow, Green, Blue, Indigo, and Violet.");
                 }
+                string transport;
+                    if (userFavColor == "red")
+            { transport = "a sparkling unicorn";
+            }
+                    else if (userFavColor == "orange")
+            { transport = "Apona";
+            }
+                    else if (userFavColor == "yellow")
+            { transport = "Buckbeak";
+            }
+                    else if (userFavColor == "green")
+            { transport = "a unicycle";
+            }
+                    else if (userFavColor == "blue")
+            { transport = "a rusty old Rambler";
+            }
+                    else if (userFavColor == "indigo")
+            { transport = "your trusty two feet";
+            }
+                    else
+            { transport = "Floo Powder";
+            }
+
 
             Console.WriteLine("And now, for the final FLOURISH, the last piece of the puzzle... \nHow many siblings do you have, my dearie?");
             int userSibs = int.Parse(Console.ReadLine().Trim());
 
-            ////PART TWO
-            //int ageModulus = userAge % 2;
-            //if (ageModulus == 0)
-            //{ int retireEven = 5;
-            //}         
+            string vacaHome;
+            if (userSibs == 0)
+                {vacaHome = "Australia";
+                }
+            else if (userSibs == 1)
+                {vacaHome = "Vancouver";
+                }
+            else if (userSibs == 2)
+                {vacaHome = "Colorado";
+                }
+            else if (userSibs == 3)
+                {vacaHome = "England";
+                }
+            else if (userSibs < 0)
+                {vacaHome = "Pennywise's Dance Studio";
+                }
+            else
+                { vacaHome = "Caribbean Islands, so you will all have your own space";
+                }
 
-            //if (userSibs == 0)
-            //{ string zeroSibs = "Australia";
-            //}
-            //else if (userSibs == 1)
-            //{ string oneSib = "Vancouver";
-            //}
-            //else if (userSibs == 2)
-            //{ string twoSibs = "Colorado";
-            //}
-            //else (userSibs == 3)
-            //{ string threeSibs = "England";
-            //}
 
-            Console.WriteLine(userFirstName + " " + userSecondName + " will retire in " + retire);
+            Console.WriteLine(userFirstName + " " + userSecondName + " will retire in " + retire + " with a vacation home in " + vacaHome);
 
 
         }
