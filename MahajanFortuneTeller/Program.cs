939324c95fb988303cafe6a9f068be5693208159
mahajanl <lakshamima@gmail.com>
2017-09-29 17:17:12 -0400

    even more finishing touches and additional complications

--- MahajanFortuneTeller/Program.cs
+++ MahajanFortuneTeller/Program.cs
@@ -14,7 +14,7 @@ namespace MahajanFortuneTeller
 
             Console.WriteLine("HELLO gentle MORTAL! I am Elvira, the Great and WONDROUS Oracle of CLEVELAND!");
             Console.WriteLine("If you'd like to gaze into the future, I can help you with that!");
-            Console.WriteLine("Would you like to gaze into the future with me?");
+            Console.WriteLine("Would you like to gaze into the future with me? Tell me YES or NO");
             string answer = Console.ReadLine().ToLower().Trim();
                 if (answer == "yes")
                 { Console.WriteLine("Let's proceed!");
